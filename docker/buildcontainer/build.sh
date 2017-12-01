f001ee894b8a53dd405687d9e0648242a823627e
Tom Wilkie <tom.wilkie@gmail.com>
2017-12-01 15:06:36 +0000

    Hack up scripts to build container.

--- docker/buildcontainer/build.sh
+++ docker/buildcontainer/build.sh
@@ -3,8 +3,8 @@
 docker kill gfbuild
 docker rm gfbuild
 
-docker build --tag "grafana/buildcontainer" docker/buildcontainer
+#docker build --tag "grafana/buildcontainer" docker/buildcontainer
 
-docker run -i -t \
-  -v /home/torkel/dev/go:/go \
-  --name gfbuild grafana/buildcontainer
+docker run -i -t --rm \
+  -v $GOPATH/src/github.com/grafana/grafana:/go/src/github.com/grafana/grafana \
+  golang:1.9.2 /bin/sh -c "cd /go/src/github.com/grafana/grafana; go run build.go setup && go run build.go build"
